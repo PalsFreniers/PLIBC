0a98b02cffe88906301b9cd309efb0609408c37a
Strong214356 <tidiandelage1@gmail.com>
2023-06-22 14:33:29 +0000

    types documentation

---
+++ types.h
@@ -0,0 +1,21 @@
+#ifndef PLIBC_TYPES_H
+#define PLIBC_TYPES_H
+
+/*
+ * Types
+ * with this lib you can use custom types in C
+ *
+ * types are rust like types
+ *
+ * here is the list of types that are available:
+ *        - integers : i8, i16, i32, i64
+ *        - unsigned : u8, u16, u32, u64
+ *        - floats   : f32, f64
+ *        - bool
+ *        - str
+ */
+
+#include "types/macros.h"
+#include "types/typedefs.h"
+
+#endif
