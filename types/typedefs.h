eab9196b02bf49debfd9017e23343f77c1bffa07
Strong214356 <tidiandelage1@gmail.com>
2023-06-22 14:33:43 +0000

    setting types and macros

---
+++ types/typedefs.h
@@ -0,0 +1,23 @@
+#ifndef PLIBC_TYPES_TYPEDEFS_H
+#define PLIBC_TYPES_TYPEDEFS_H
+
+typedef char i8;
+typedef short int i16;
+typedef int i32;
+typedef long long int i64;
+
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long long int u64;
+
+typedef u64 size_t;
+
+typedef float  f32;
+typedef double f64;
+
+typedef int bool;
+
+typedef char* str;
+
+#endif
