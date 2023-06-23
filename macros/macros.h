c0d528857e3260ef44cc98d5b15782f3fa155c0f
Strong214356 <tidiandelage1@gmail.com>
2023-06-23 12:37:58 +0000

    fucking .gitignore

---
+++ macros/macros.h
@@ -0,0 +1,14 @@
+#ifndef PLIBC_MACROS_MACROS_H
+#define PLIBC_MACROS_MACROS_H
+
+#define UNUSED(x) ((void)x)
+#define TODO(str) {\
+                  fprintf("[TODO] %s -> %s", __FILE__, str);\
+                  exit(1);\
+                  }
+#define BIT(x) (1 << x)
+#define KILOBYTE(x) (x * 1024)
+#define MEGABYTE(x) (KILOBYTE(x) * 1024)
+#define GIGABYTE(x) (MEGABYTE(x) * 1024)
+
+#endif
