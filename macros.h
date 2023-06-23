e4a770ca4cabb4d661db1e92164f71f4f2090e68
Strong214356 <tidiandelage1@gmail.com>
2023-06-23 12:36:08 +0000

    writing macros

---
+++ macros.h
@@ -0,0 +1,20 @@
+#ifndef PLIBC_MACROS_H
+#define PLIBC_MACROS_H
+
+/*
+ * Macros
+ * with this lib you will be given simple generic macros
+ * here is the list of all macros
+ *         - UNUSED(x); use this macro to set it unused (for compile warnings)
+ *         - TODO(str); exit program saying "[TODO] <file> -> str"
+ *         - BIT(x); create a mask on bit x
+ *         - KILOBYTE(x); convert x kilobyte to bytes
+ *         - MEGABYTE(x); convert x megabyte to bytes
+ *         - GIGABYTE(x); convert x gigabyte to bytes
+ */
+
+#include <stdlib.h>
+
+#include "macros/macros.h"
+
+#endif
