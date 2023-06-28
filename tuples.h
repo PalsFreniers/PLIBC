b25ca899f175c6e1fd362483f380f8bfd24fb0c1
Strong214356 <tidiandelage1@gmail.com>
2023-06-28 07:23:41 +0000

    creating basic documentation

---
+++ tuples.h
@@ -0,0 +1,22 @@
+#ifndef PLIBC_TUPLE_H
+#define PLIBC_TUPLE_H
+
+/*
+ * Tuples
+ * with this lib you can use generic tuples in C
+ *
+ * 
+ * to use a tuple, first instantiate tuple struct using :
+ * struct tuple *tupleName = Tuple_create();
+ *
+ * tuples have dynamic size but can be resized manually using the function:
+ * Tuple_resize(*tuple, size);
+ * 
+ * then to set and/or get values in the tuple, you can use many function (one for each type) :
+ * Tuple_set<type>(*tuple, value, index); // replace <type> by the type that you wan't to use
+ * Tuple_get<type>(*tuple, value, index); // replace <type> by the type that you wan't to use
+ * 
+ * see types.h to get the type names
+ */
+
+#endif
