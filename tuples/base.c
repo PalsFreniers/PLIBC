e1ab5b1767707d49a1228d8bdbd3d937d0ddf672
Strong214356 <tidiandelage1@gmail.com>
2023-07-03 23:04:48 +0200

    adding all getters and setters functions

--- tuples/base.c
+++ tuples/base.c
@@ -13,46 +13,134 @@ struct tuple *Tuple_create() {
 }
 
 bool Tuple_resize(struct tuple *tuple, size_t newSize) {
+    if(tuple->size == newSize) return true;
     void *tmp = malloc(newSize * sizeof(void *));
     if(tmp == NULL) return false;
-    char *t = memcpy(tmp, tuple->arr, );
-    if(t == NULL);
+    char *t = memcpy(tmp, tuple->arr, MIN(newSize, tuple->size));
+    if(t == NULL) return false;
     free(tuple->arr);
     tuple->arr = tmp;
     return true;
 }
 
-void Tuple_destroy(struct tuple *tuple);
+void Tuple_destroy(struct tuple *tuple) {
+    if(tuple != NULL && tuple->arr != NULL) free(tuple->arr);
+    if(tuple != NULL) free(tuple);
+}
 
 // setters
-void Tuple_setI8(struct tuple *tuple, i8 data, size_t index);
-void Tuple_setI16(struct tuple *tuple, i16 data, size_t index);
-void Tuple_setI32(struct tuple *tuple, i32 data, size_t index);
-void Tuple_setI64(struct tuple *tuple, i64 data, size_t index);
+void Tuple_setI8(struct tuple *tuple, i8 *data, size_t index) {
+    if(tuple->size < index) Tuple_resize(tuple, index + 1);
+    tuple->arr[index] = (void *)data;
+}
 
-void Tuple_setU8(struct tuple *tuple, u8 data, size_t index);
-void Tuple_setU16(struct tuple *tuple, u16 data, size_t index);
-void Tuple_setU32(struct tuple *tuple, u32 data, size_t index);
-void Tuple_setU64(struct tuple *tuple, u64 data, size_t index);
+void Tuple_setI16(struct tuple *tuple, i16 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
 
-void Tuple_setF32(struct tuple *tuple, f32 data, size_t index);
-void Tuple_setF64(struct tuple *tuple, f64 data, size_t index);
+void Tuple_setI32(struct tuple *tuple, i32 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
 
-void Tuple_setBool(struct tuple *tuple, bool data, size_t index);
-void Tuple_setStr(struct tuple *tuple, str data, size_t index);
-// getters
-void Tuple_getI8(struct tuple *tuple, i8 data, size_t index);
-void Tuple_getI16(struct tuple *tuple, i16 data, size_t index);
-void Tuple_getI32(struct tuple *tuple, i32 data, size_t index);
-void Tuple_getI64(struct tuple *tuple, i64 data, size_t index);
+void Tuple_setI64(struct tuple *tuple, i64 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
 
-void Tuple_getU8(struct tuple *tuple, u8 data, size_t index);
-void Tuple_getU16(struct tuple *tuple, u16 data, size_t index);
-void Tuple_getU32(struct tuple *tuple, u32 data, size_t index);
-void Tuple_getU64(struct tuple *tuple, u64 data, size_t index);
+ 
+void Tuple_setU8(struct tuple *tuple, u8 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
 
-void Tuple_getF32(struct tuple *tuple, f32 data, size_t index);
-void Tuple_getF64(struct tuple *tuple, f64 data, size_t index);
+void Tuple_setU16(struct tuple *tuple, u16 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
 
-void Tuple_getBool(struct tuple *tuple, bool data, size_t index);
-void Tuple_getStr(struct tuple *tuple, str data, size_t index);
+void Tuple_setU32(struct tuple *tuple, u32 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
+
+void Tuple_setU64(struct tuple *tuple, u64 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
+
+ 
+void Tuple_setF32(struct tuple *tuple, f32 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
+
+void Tuple_setF64(struct tuple *tuple, f64 *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
+
+ 
+void Tuple_setBool(struct tuple *tuple, bool *data, size_t index) { 
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
+
+void Tuple_setStr(struct tuple *tuple, str *data, size_t index) {  
+    if(tuple->size < index) Tuple_resize(tuple, index + 1); 
+    tuple->arr[index] = (void *)data; 
+}
+
+// getters 
+i8 Tuple_getI8(struct tuple *tuple, size_t index) {
+    return *(i8 *)tuple->arr[index];
+}
+
+i16 Tuple_getI16(struct tuple *tuple, size_t index) {
+    return *(i16 *)tuple->arr[index]; 
+} 
+
+i32 Tuple_getI32(struct tuple *tuple, size_t index) {
+    return *(i32 *)tuple->arr[index]; 
+} 
+
+i64 Tuple_getI64(struct tuple *tuple, size_t index) {
+    return *(i64 *)tuple->arr[index]; 
+} 
+  
+ 
+u8 Tuple_getU8(struct tuple *tuple, size_t index) {
+    return *(u8 *)tuple->arr[index]; 
+} 
+
+u16 Tuple_getU16(struct tuple *tuple, size_t index) {
+    return *(u16 *)tuple->arr[index]; 
+} 
+
+u32 Tuple_getU32(struct tuple *tuple, size_t index) {
+    return *(u32 *)tuple->arr[index]; 
+} 
+
+u64 Tuple_getU64(struct tuple *tuple, size_t index) {
+    return *(u64 *)tuple->arr[index]; 
+} 
+  
+
+f32 Tuple_getF32(struct tuple *tuple, size_t index) {
+    return *(f32 *)tuple->arr[index]; 
+} 
+
+f64 Tuple_getF64(struct tuple *tuple, size_t index) {
+    return *(f64 *)tuple->arr[index]; 
+} 
+  
+
+bool Tuple_getBool(struct tuple *tuple, size_t index) {
+    return *(bool *)tuple->arr[index]; 
+} 
+
+str Tuple_getStr(struct tuple *tuple, size_t index) {
+    return *(str *)tuple->arr[index]; 
+} 
+  
--- tuples/base.h
+++ tuples/base.h
@@ -4,9 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../types.h"
+#include "../macros.h"
 
 struct tuple {
-    void* arr;
+    void** arr;
     size_t size;
 };
 
@@ -15,36 +16,36 @@ bool Tuple_resize(struct tuple *tuple, size_t newSize);
 void Tuple_destroy(struct tuple *tuple);
 
 // setters
-void Tuple_setI8(struct tuple *tuple, i8 data, size_t index);
-void Tuple_setI16(struct tuple *tuple, i16 data, size_t index);
-void Tuple_setI32(struct tuple *tuple, i32 data, size_t index);
-void Tuple_setI64(struct tuple *tuple, i64 data, size_t index);
+void Tuple_setI8(struct tuple *tuple, i8 *data, size_t index);
+void Tuple_setI16(struct tuple *tuple, i16 *data, size_t index);
+void Tuple_setI32(struct tuple *tuple, i32 *data, size_t index);
+void Tuple_setI64(struct tuple *tuple, i64 *data, size_t index);
 
-void Tuple_setU8(struct tuple *tuple, u8 data, size_t index);
-void Tuple_setU16(struct tuple *tuple, u16 data, size_t index);
-void Tuple_setU32(struct tuple *tuple, u32 data, size_t index);
-void Tuple_setU64(struct tuple *tuple, u64 data, size_t index);
+void Tuple_setU8(struct tuple *tuple, u8 *data, size_t index);
+void Tuple_setU16(struct tuple *tuple, u16 *data, size_t index);
+void Tuple_setU32(struct tuple *tuple, u32 *data, size_t index);
+void Tuple_setU64(struct tuple *tuple, u64 *data, size_t index);
 
-void Tuple_setF32(struct tuple *tuple, f32 data, size_t index);
-void Tuple_setF64(struct tuple *tuple, f64 data, size_t index);
+void Tuple_setF32(struct tuple *tuple, f32 *data, size_t index);
+void Tuple_setF64(struct tuple *tuple, f64 *data, size_t index);
 
-void Tuple_setBool(struct tuple *tuple, bool data, size_t index);
-void Tuple_setStr(struct tuple *tuple, str data, size_t index);
+void Tuple_setBool(struct tuple *tuple, bool *data, size_t index);
+void Tuple_setStr(struct tuple *tuple, str *data, size_t index);
 // getters
-void Tuple_getI8(struct tuple *tuple, i8 data, size_t index);
-void Tuple_getI16(struct tuple *tuple, i16 data, size_t index);
-void Tuple_getI32(struct tuple *tuple, i32 data, size_t index);
-void Tuple_getI64(struct tuple *tuple, i64 data, size_t index);
+i8 Tuple_getI8(struct tuple *tuple, size_t index);
+i16 Tuple_getI16(struct tuple *tuple, size_t index);
+i32 Tuple_getI32(struct tuple *tuple, size_t index);
+i64 Tuple_getI64(struct tuple *tuple, size_t index);
+    
+u8 Tuple_getU8(struct tuple *tuple, size_t index);
+u16 Tuple_getU16(struct tuple *tuple, size_t index);
+u32 Tuple_getU32(struct tuple *tuple, size_t index);
+u64 Tuple_getU64(struct tuple *tuple, size_t index);
+  
+f32 Tuple_getF32(struct tuple *tuple, size_t index);
+f64 Tuple_getF64(struct tuple *tuple, size_t index);
 
-void Tuple_getU8(struct tuple *tuple, u8 data, size_t index);
-void Tuple_getU16(struct tuple *tuple, u16 data, size_t index);
-void Tuple_getU32(struct tuple *tuple, u32 data, size_t index);
-void Tuple_getU64(struct tuple *tuple, u64 data, size_t index);
-
-void Tuple_getF32(struct tuple *tuple, f32 data, size_t index);
-void Tuple_getF64(struct tuple *tuple, f64 data, size_t index);
-
-void Tuple_getBool(struct tuple *tuple, bool data, size_t index);
-void Tuple_getStr(struct tuple *tuple, str data, size_t index);
+bool Tuple_getBool(struct tuple *tuple, size_t index);
+str Tuple_getStr(struct tuple *tuple, size_t index);
 
 #endif
