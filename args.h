2414c2ab002e43ec043f241d1375d297f45877e0
Strong214356 <tidiandelage1@gmail.com>
2023-08-10 22:24:04 +0200

    tying to add libc to PLIBC

---
+++ args.h
@@ -0,0 +1,6 @@
+#ifndef PLIBC_ARGS_H
+#define PLIBC_ARGS_H
+
+#include "args/macros.h"
+
+#endif
---
+++ io/base.h
@@ -0,0 +1,10 @@
+#ifndef PLIBC_IO_BASE_H
+#define PLIBC_IO_BASE_H
+
+#include <types.h>
+
+void putc(char c);
+void puts(const str s);
+void printf(const str fmt, )
+
+#endif // PLIBC_IO_BASE_H
